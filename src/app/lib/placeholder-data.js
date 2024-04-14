ccd47c655363cce28a03566fdd9bf631f67b202e
AlannaParsons <alannagparsons@gmail.com>
2024-04-13 21:53:30 -0600

    added user table and seed data for db

---
+++ src/app/lib/placeholder-data.js
@@ -0,0 +1,38 @@
+// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
+// https://nextjs.org/learn/dashboard-app/fetching-data
+const users = [
+  {
+    id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
+    name: 'McGraw, Phil',
+    designation: 'DR',
+    address: 'Vinita, Oklahoma',
+    phone_number: '88500',
+    email: 'p.mcgraw@gmail.com',
+    password: 'dogdonthunt',
+    company_name: 'Courtroom Sciences, Inc.',
+    website: 'www.drphil.com',
+    accepting_clients: 't',
+    price_min: 100,
+    price_max: 200,
+    therapies: 'clinical, forensic'
+  },
+  {
+    id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
+    name: 'Pinsky, Drew',
+    designation: 'DR',
+    address: 'Pasadena, California',
+    phone_number: '111-1111',
+    email: 'd.pinsky@gmail.com',
+    password: 'askdrdrew',
+    company_name: 'Sober House',
+    website: 'https://drdrew.com/',
+    accepting_clients: 'f',
+    price_min: 1500,
+    price_max: 2500,
+    therapies: 'addiction'
+  },
+];
+
+module.exports = {
+  users
+};
